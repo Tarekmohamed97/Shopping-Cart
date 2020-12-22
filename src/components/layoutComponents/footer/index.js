f919821fff335bd9d724db00aa133832ce57a17e
Tarekmohamed97 <tm7271420@gmail.com>
2020-12-22 18:55:03 +0200

    starting of with some basi setup for the system

---
+++ src/components/layoutComponents/footer/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+    return (
+        <footer className = "Tasweek__footer__container">
+            All Rights reserved to TASWEEK team &copy; 2020
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/layoutComponents/header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./header.css"
+
+const Header = () => {
+    return (
+        <header className = "Tasweek__Header__container">
+          <a href = "/">TASWEEK</a>
+        </header>
+    )
+}
+
+export default Header
