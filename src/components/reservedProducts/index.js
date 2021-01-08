98656d4d8f4dff0ca0bc88502f283b19194a34b0
Tarekmohamed97 <tm7271420@gmail.com>
2021-01-08 14:33:30 +0200

    adding the form inputs and styling to the userForm pop up

--- src/components/reservedProducts/index.js
+++ src/components/reservedProducts/index.js
@@ -1,43 +1,50 @@
-import React from 'react';
+import React, {useState} from 'react';
 import formatCurrency from '../../utilities';
 import './reservedProducts.css'
 
-const ReservedProducts = ({cartItems, handleRemoveFromCart, totalCost}) => {
+const ReservedProducts = ({cartItems, handleRemoveFromCart, totalCost, setShowUserFormPopup}) => {
+
     return (
         <div>
-            <div className = "cartHeader__section">
-            {
-                cartItems.length === 0?   
-                <div className = "cartHeader__text">Your cart is empty</div>
-                :
-                <div className = "cartHeader__text">you have {cartItems.length} in the Cart {" "}</div>
-            }
-            </div>
             <div>
+                <div className = "cartHeader__section">
                 {
-                    cartItems.map(item => (
-                        <div className = "reservedProduct__container" key = {item._id}>
-                            <div>
-                                <img className = "reservedProducts__image" src = {item.image} alt = {item.title} />
-                            </div>
-                            <div className = "reservedProducts__dataSection">
-                                <div>{item.title}</div>
-                                <div> Price:  {item.count} x {formatCurrency(item.price)} </div>
-                                <button className = "removeFromCart__button" onClick = {() => handleRemoveFromCart(item)}>Remove Item</button>
-                            </div>
-                        </div>
-                    ))
+                    cartItems.length === 0?   
+                    <div className = "cartHeader__text">Your cart is empty</div>
+                    :
+                    <div className = "cartHeader__text">you have {cartItems.length} in the Cart {" "}</div>
                 }
-                {
-                    cartItems.length !== 0 && (
-                        <div className = "costProceed__Section">
-                            <div>Total Cost: {formatCurrency(totalCost)}</div>
-                            <div>
-                                <button className = "ProceedButton__Section" >Proceed</button>
+                </div>
+                <div>
+                    {
+                        cartItems.map(item => (
+                            <div className = "reservedProduct__container" key = {item._id}>
+                                <div>
+                                    <img className = "reservedProducts__image" src = {item.image} alt = {item.title} />
+                                </div>
+                                <div className = "reservedProducts__dataSection">
+                                    <div>{item.title}</div>
+                                    <div> Price:  {item.count} x {formatCurrency(item.price)} </div>
+                                    <button className = "removeFromCart__button" onClick = {() => handleRemoveFromCart(item)}>Remove Item</button>
+                                </div>
                             </div>
-                        </div>
-                    )
-                }
+                        ))
+                    }
+                    {
+                        cartItems.length !== 0 && (
+                            <div className = "costProceed__Section">
+                                <div>Total Cost: {formatCurrency(totalCost)}</div>
+                                <div>
+                                    <button
+                                        className = "ProceedButton__Section"
+                                        onClick =  {() => setShowUserFormPopup(true)}>
+                                            Proceed
+                                    </button>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
             </div>
         </div>
     )
