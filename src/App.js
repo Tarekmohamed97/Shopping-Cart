7d47f7d004b048b3e06a20e0eb17b238759209aa
Tarekmohamed97 <tm7271420@gmail.com>
2021-01-08 16:46:15 +0200

    adding the store and the browser router components to the app.js file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import React, {useState} from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import data from "./data.json";
 import Header from './components/layoutComponents/header';
 import Footer from './components/layoutComponents/footer'
 import ListOfProducts from './components/listOfProducts';
+import ConfirmationPage from './components/confirmationPage'
+import store from './redux/store';
+import {Provider} from 'react-redux';
+
+
 
 function App() {
 
   return (
-    <div className="App">
+    <BrowserRouter>
+    <Provider store = {store}>
+      <div className="App">
         <Header />
-        <ListOfProducts />
+        <Switch>
+          <Route path = "/" exact component = {ListOfProducts} />
+          <Route path = "/confirmationPage" exact component = {ConfirmationPage} />
+        </Switch>
         <Footer />
-    </div>
+      </div>
+    </Provider>
+    </BrowserRouter>
   );
 }
 
